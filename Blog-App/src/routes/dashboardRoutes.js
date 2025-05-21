9ddbf5171e4af64401524e19d44a543b707478f4
Adriyansah Syaputra <adriansyahputra2626@gmail.com>
2025-05-21 19:01:31 +0700

    change links

--- Blog-App/src/routes/dashboardRoutes.js
+++ Blog-App/src/routes/dashboardRoutes.js
@@ -9,15 +9,15 @@ router.get("/posts", (req, res) => {
     res.render("./pages/dashboard/posts", { title: "Posts" });
 })
 
-router.get("/add-post", (req, res) => {
+router.get("/posts/new", (req, res) => {
     res.render("./pages/dashboard/add-post", { title: "Add Post" });
 })
 
-router.get("/edit-post", (req, res) => {
+router.get("/posts/edit", (req, res) => {
     res.render("./pages/dashboard/edit-post", { title: "Edit Post" });
 })
 
-router.get("/detail-post", (req, res) => {
+router.get("/posts/detail", (req, res) => {
     res.render("./pages/dashboard/detail-post", { title: "Detail Post" });
 })
 
